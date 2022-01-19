2159c7a2b02a027668728dd3f544556f2347d224
sadose <sadose@qq.com>
2022-01-19 20:40:38 +0800

    完成了壁纸组件

---
+++ src/components/Wallpaper/index.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+import { Wallpapers } from "../../definitions/ModelsDef";
+import { ReduxState } from "../../definitions/ReduxDef";
+
+import "./index.scss";
+
+export default function Wallpaper() {
+  const userWallpaperSetting: Wallpapers = useSelector<ReduxState, any>(
+    (state) => state.changeWallpaper
+  );
+  const wallpaperBlurState: boolean = useSelector<ReduxState, any>((state) => state.isSearching);
+
+  // 获取用户设置的壁纸
+  const backgroundImageName: Wallpapers = userWallpaperSetting;
+  // 获取壁纸图片
+  // Todo: 判断是否为用户自定义壁纸
+  const backgroundImageUrl: String = require("./assets/" + backgroundImageName + ".jpg");
+
+  return (
+    <div
+      className="Wallpaper"
+      data-searching={wallpaperBlurState ? "true" : "false"}
+      style={{
+        backgroundImage: "url(" + backgroundImageUrl + ")",
+      }}
+    ></div>
+  );
+}
