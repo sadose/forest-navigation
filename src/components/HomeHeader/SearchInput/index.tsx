5b6be56a10449585aacca7884fbb7ed7af508dba
sadose <sadose@qq.com>
2022-03-28 11:58:20 +0800

    优化了搜索框逻辑

--- src/components/HomeHeader/SearchInput/index.tsx
+++ src/components/HomeHeader/SearchInput/index.tsx
@@ -1,57 +1,55 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+
+import { useDidUpdateEffect, useDebounce } from "../../../hooks";
 
 import { ReduxState } from "../../../definitions/ReduxDef";
 
-import { isSearchingCreater } from "../../../redux/actions";
+import { isSearchingCreater, searchTextCreater } from "../../../redux/actions";
 
 import { getDefaultSearchUrl } from "../../../utils/userSettings/getUserSettings";
-
-import useInputValue from "./hooks/useInputValue";
 
 import "./index.scss";
 
 export default function SearchInput() {
-  const isSearching = useSelector<ReduxState, any>((state) => state.isSearching);
-  const [inputPlaceholder, setInputPlaceholder] = useState("在此处搜索");
-  const [inputValue, setInputValue, lastValue] = useInputValue("");
-  const inputRef = useRef<HTMLInputElement | null>(null);
   const dispatch = useDispatch();
-  useEffect(() => {
-    if (!inputValue && lastValue) {
+  const isSearching = useSelector<ReduxState, any>((state) => state.isSearching);
+  const inputValue = useSelector<ReduxState, any>((state) => state.searchText);
+
+  const [currentInputValue, setCurrentInputValue] = useState("");
+
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  useDebounce(() => dispatch(searchTextCreater(currentInputValue)), 2000, [currentInputValue]);
+
+  useDidUpdateEffect(() => {
+    if (!inputValue) {
       inputRef.current && inputRef.current.focus();
+      setCurrentInputValue(inputValue); // 唯一可能由外部改变 inputValue 的情况就是按下了清空按钮
     }
   }, [inputValue]);
 
   const defaultSearchUrl = getDefaultSearchUrl();
 
+  console.log(currentInputValue);
+
   return (
     <div className="SearchInput">
       <input
         type="text"
-        placeholder={inputPlaceholder}
-        value={inputValue}
+        placeholder={isSearching ? "" : "在此处搜索"}
+        value={currentInputValue}
         ref={inputRef}
-        onFocus={(e) => {
-          const inp = e.target as HTMLInputElement;
-          if (inp.value) {
-          } else {
-            setInputPlaceholder("");
-          }
-          dispatch(isSearchingCreater(true));
+        onFocus={() => {
+          if (!isSearching) dispatch(isSearchingCreater(true));
         }}
         onBlur={(e) => {
           const inp = e.target as HTMLInputElement;
-          if (inp.value) {
-          } else {
-            setInputPlaceholder("在此处搜索");
-            dispatch(isSearchingCreater(false));
-          }
+          if (!inp.value) dispatch(isSearchingCreater(false));
         }}
         onInput={(e) => {
-          // Todo: 应当做防抖
           const inp = e.target as HTMLInputElement;
-          setInputValue(inp.value);
+          setCurrentInputValue(inp.value);
         }}
         onKeyPress={(e) => {
           const inp = e.target as HTMLInputElement;
